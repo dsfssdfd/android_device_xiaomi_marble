feade7482d6e2af87ea11eaeefd34fb8551e7823
Dyneteve <sound0020@gmail.com>
2023-05-01 17:34:08 -0300

    marble: Enable AOSP surfaceflinger
    - AOSP SurfaceFlinger is smoother than CAF
    
    Change-Id: Id671da2363fb09303a7d7241d5ae95e55ff16475
    Signed-off-by: RobertGarciaa <chae0218@naver.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -230,6 +230,9 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy/public/xiaomi/devicesettings
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # VINTF
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_SKUS := ukee
